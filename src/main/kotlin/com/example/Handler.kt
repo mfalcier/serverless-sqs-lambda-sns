34b0c8b3c59c796e469cfa7fccc8e5fdd8ae3515
mfalcier <marcofalcier@gmail.com>
2017-12-21 17:45:59 +0100

    :bug: Fixed SQS message deletion

--- src/main/kotlin/com/example/Handler.kt
+++ src/main/kotlin/com/example/Handler.kt
@@ -2,29 +2,40 @@ package com.example
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
-import org.slf4j.LoggerFactory
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest
 import com.amazonaws.services.sns.AmazonSNSClientBuilder
 import com.amazonaws.services.sns.model.PublishRequest
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 import com.amazonaws.services.sqs.model.DeleteMessageRequest
-import com.sun.org.apache.xml.internal.serializer.utils.Utils.messages
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest
+import org.slf4j.LoggerFactory
 
 class Handler:RequestHandler<Map<String, Any>, Any> {
     override fun handleRequest(input:Map<String, Any>, context:Context):Any {
-
+        // Initializing SQS client
         val sqs = AmazonSQSClientBuilder.defaultClient()
-        val url = sqs.getQueueUrl("serverless-sqs-lambda-sns-dev-queue").queueUrl
+        val sqsUrl = System.getenv("sqsUrl")
 
-        val receiveMessageRequest = ReceiveMessageRequest(url)
+        // Creating SQS Request with timeout
+        val receiveMessageRequest = ReceiveMessageRequest(sqsUrl)
+        receiveMessageRequest.visibilityTimeout = 600
+
+        // Retrieving messages and processing them
         val messages = sqs.receiveMessage(receiveMessageRequest).messages
         for (message in messages) {
             LOG.info("Message: {}", message.body)
 
+            // Initializing SNS client
             val sns = AmazonSNSClientBuilder.defaultClient()
-            val topic = sns.createTopic("serverless-sqs-lambda-sns-dev-topic")
-            val publishRequest = PublishRequest(topic.topicArn, message.body)
+            val snsArn = System.getenv("snsArn")
+
+            // Creating topic request and publishing it
+            val publishRequest = PublishRequest(snsArn, message.body)
             val publishResult = sns.publish(publishRequest)
+
+            // Deleting SQS Message
+            val messageReceiptHandle = message.receiptHandle
+            sqs.deleteMessage(DeleteMessageRequest(sqsUrl, messageReceiptHandle))
+
             LOG.info("Topic Result: {}", publishResult)
         }
         return Any()
